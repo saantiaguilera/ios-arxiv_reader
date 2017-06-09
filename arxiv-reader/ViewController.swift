f1593b12c580d29d4b7b070c609ccaa28b68e836
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-06-09 20:23:38 -0300

    init

---
+++ arxiv-reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  arxiv-reader
+//
+//  Created by Santiago Aguilera on 6/9/17.
+//  Copyright © 2017 Santiago Aguilera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
